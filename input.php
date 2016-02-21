2fdf4a488987387c795c091e42e72c2c3320f7e5
Serenaise <serenaise@gmail.com>
2016-02-21 15:29:00 -0700

    Added files via upload

--- input.php
+++ input.php
@@ -2,46 +2,175 @@
   $counter = $_COOKIE['counter'];
 	//setcookie("test_cookie", $_COOKIE('test_cookie'), time() + 3600, '/');
 ?>
-
+<!DOCTYPE html>
 <html>
-<header>
-  <title>Wearther or Not </title>
-  <meta charset="UTF-8">
-<style>
-  body {
-    background: url(http://vectorandpsd.com/vectorandpsd/background/blue-poly-background.jpg);
-    background-size: cover 100%;
-    background-repeat: no-repeat;
-    }
-    #day{
-    }
-    </style>
-  	</header>
-  	<body>
-      <form method="post" action="index.php">
-        Enter a new latitude and longitude:
-     		 <input type = "submit" id="button" style= "color: #cc99ff; background-color: #0066ff; opacity: 0.5;" >
-
-      </form>
-<?php
-/*ignore this, for the later file*/
-//echo "ARRAY ".$_SESSION["coatArray"][0]." COUNTER: ".$_SESSION["counter"];
-htmlspecialchars($_SERVER["PHP_SELF"]);
-$lat = "40.02";
-$lon = "-105.28";
-/*if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-	//$lat = test_input($_POST['lat']);
-	//$lon = test_input($_POST['lon']);
-	//echo $lat;
-	//echo $lon;
-}*/
-$lessthanzero=null;
-$zerotoforty=null;
-$fortytosixty=null;
-$sixtytoeighty=null;
-$eightyplus=null;
-for($nn=0; $nn<$counter; $nn++){
+	<head>
+	  <title>Wearther or Not </title>
+	  <meta charset="UTF-8">
+	  <link href='https://fonts.googleapis.com/css?family=Montserrat|Merriweather' rel='stylesheet' type='text/css'>
+	<style>
+		body {
+			background: url(http://vectorandpsd.com/vectorandpsd/background/blue-poly-background.jpg);
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
+			border: 2px solid black;
+			font-family: 'Merriweather', serif;
+		}
+		h1, h2, h3, h4 {
+			font-family: 'Montserrat', sans-serif;
+		}
+		h1 {
+			text-align: center;
+			font-size: 50px;
+			color: #fff;
+			text-transform: uppercase;
+			letter-spacing: 4px;
+		}
+		#day {
+			font-size: 30px;
+			color: #4d3319;
+			text-align: center;
+		}
+		.button {
+			background-color: #4CAF50; /* Green */
+			border: none;
+			color: white;
+			padding: 16px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: block;
+			font-size: 16px;
+			margin: 10px auto 50px;
+			-webkit-transition-duration: 0.4s; /* Safari */
+			transition-duration: 0.4s;
+			cursor: pointer;
+		}
+		.button2 {
+			background-color: #e6ccb3; 
+			color: #333; 
+			border-radius: 5px;
+		}
+		.button2:hover {
+			background-color: #d2a479;
+		}
+		#temp {
+			text-align: center;
+		}
+		#temperature {
+			text-align: center;
+			font-size: 80px;
+		}
+		hr {
+			display: block;
+			height: 2px;
+			border: 0;
+			border-top: 2px solid black;
+			margin: 1em 0;
+			padding: 0; 
+		}
+		#text {
+			text-align: center;
+			width: 90%;
+			max-width: 700px;
+			margin: 0 auto;
+		}
+		.vidcover {
+		  background: #000;
+		  opacity: 0;
+		  display: block;
+		  width: 100%;
+		  height: 100%;
+		  position: fixed;
+		  top: 0;
+		  left: 0;
+		  z-index: -1;
+		  -webkit-transition: opacity 800ms ease 0.2s;
+		  -moz-transition: opacity 800ms ease 0.2s;
+		  -ms-transition: opacity 800ms ease 0.2s;
+		  transition: opacity 800ms ease 0.2s;
+		}
+		.fullvid {
+		  width: 1280px;
+		  height: 720px;
+		  position: fixed;
+		  bottom: 50%;
+		  left: 50%;
+		  z-index: -2;
+		  -webkit-transform: translate(-50%, 50%);
+		  -moz-transform: translate(-50%, 50%);
+		  -ms-transform: translate(-50%, 50%);
+		  transform: translate(-50%, 50%);
+		  -webkit-transition: all 400ms ease-out 400ms;
+		  -moz-transition: all 400ms ease-out 400ms;
+		  -ms-transition: all 400ms ease-out 400ms;
+		  transition: all 400ms ease-out 400ms;
+		}
+		section {
+			background: #fff;
+			padding: 30px 20px;
+			width: 90%;
+			max-width: 600px;
+			margin: 0 auto;
+			border-top: 15px solid #4d3319;
+		}
+	</style>
+	<script>
+		$(document).ready(function() {
+  $('body')
+    .prepend('<div class="vidcover"></div>')
+    .hover (
+  		function() {
+  			$('.vidcover').css({'opacity':'0'})
+ 	 	},
+ 	 	function() {
+ 	 		$('.vidcover').css({'opacity':'0'})
+ 	 	}
+  	);
+  
+	$(window).resize( function(){
+		var theWidth = $(window).width();
+		var theHeight = $(window).height();
+		var newWidth = (theHeight*1.77777778);
+		var newHeight = (theWidth/1.77777778);
+		if ( (theWidth > 1280) && (newHeight > theHeight )) {
+			$('.fullvid').css({'width':theWidth, 'height':newHeight});
+		}
+		if ( (theHeight > 720) && (newWidth > theWidth )) {
+			$('.fullvid').css({'height':theHeight, 'width':newWidth});
+		}
+		$('.vidcover').css({'height':theHeight, 'width':theWidth});
+	}).resize();
+});
+	</script>
+	</head>
+	
+	<body>
+		<header>
+			<iframe class="fullvid" width="1280" height="720" src="https://www.youtube.com/embed/sHgOn5s-MCw?hd=1&amp;iv_load_policy=3&amp;loop=1&amp;rel=0&amp;showinfo=0&amp;autoplay=1&amp;controls=0" frameborder="0" allowfullscreen></iframe>
+			<h1> Wearther or Not </h1>
+		</header>
+		<form method="post" action="index.php">
+				<input type="submit" class="button button2" value="Change Location">
+		  </form>
+	<?php
+		htmlspecialchars($_SERVER["PHP_SELF"]);
+		$lat = $lon = "";
+		
+		$lessthanzero=null;
+		$zerotoforty=null;
+		$fortytosixty=null;
+		$sixtytoeighty=null;
+		$eightyplus=null;
+		
+		if ($_SERVER["REQUEST_METHOD"] == "POST")
+		{
+			$lat = test_input($_POST['lat']);
+			$lon = test_input($_POST['lon']);
+			//echo $lat;
+			//echo $lon;
+		}
+		
+		for($nn=0; $nn<$counter; $nn++){
   $arr=$_SESSION["coatArray"][$nn];
   $tr=$arr[1];
   //echo "Temp range: ".$tr.'<br>';
@@ -83,159 +212,97 @@ for($nn=0; $nn<$counter; $nn++){
     $eightyplus[$next]=$arr[0];
   }
 }
-function test_input($data)
-{
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-}
-//var_dump($_POST);
-//if(isset($_POST['lat'])&&isset($_POST['lon'])){
-$lat = $_SESSION['lat'];
-$lon = $_SESSION['lon'];
-//}
-$LatvsLon = unserialize($_COOKIE['test_cookie']);
+		
+		function test_input($data)
+		{
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+		//var_dump($_POST);
+		$lat = $_POST['lat'];
+		$lon = $_POST['lon'];
+		//echo $lat;
+		//echo $lon;
+		$LatvsLon = unserialize($_COOKIE['test_cookie']);
 $LatvsLon[0] = $lat;
 $LatvsLon[1] = $lon;
 setcookie("test_cookie", serialize($LatvsLon), time()+3600, '/');
-//echo $lat;
-//echo $lon;
-$url="http://forecast.weather.gov/MapClick.php?lat=".$lat."&lon=".$lon."&FcstType=json";
-//echo "URL: ".$url."<br>";
-$curl_handle=curl_init();
-curl_setopt($curl_handle, CURLOPT_URL,$url);
-curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
-curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
-$query = curl_exec($curl_handle);
-curl_close($curl_handle);
-//echo $query;
-$Jweather = json_decode($query);
-$startPeriodArray = $Jweather->time->startPeriodName;
-$temp = $Jweather->data->temperature;
-$weather = $Jweather->data->weather;
-$icon = $Jweather->data->iconLink;
-$size = sizeof($startPeriodArray);
-$text = $Jweather->data->text;
-$tempId = $Jweather->time->tempLabel;
-$endArray = array(null, null, null, null, null, null, null);
-$newArray;
-$counter=0;
-for($i = 0; $i < 12; $i++){
-  $newArray = array($startPeriodArray[$i], $temp[$i], $weather[$i], $icon[$i], $text[$i]);
-  $endArray[$i] = $newArray;
-}
-for( $m=0; $m<8; $m++){
- for($r=0; $r<10; $r++){
-   if($r == 0){
-     echo '<id = "day"><b>'.$endArray[$m][$r]."</b><br>";
-   }
-   if($r == 1){
-    echo $tempId[$m], ": ";
-     echo '<id = "temperature">'.$endArray[$m][$r]."<br>";
-     $currTemp=$endArray[$m][$r];
-     if($currTemp<=0){
-       echo "Wear a heavy coat"."<br>";
-	   if($lessthanzero!=null){
-       echo "****".'<br>';
-       echo "Your appropriate coats:".'<br>';
-       for($aa=0; $aa<count($lessthanzero); $aa++){
-         echo $lessthanzero[$aa];
-         if($aa!=count($lessthanzero)-1){
-           echo ", ";
-         }
-         else {
-           echo ""."<br>";
-         }
-       }
-	   }
-     }
-     elseif($currTemp>0&&$currTemp<=40){
-       echo "wear a jacket"."<br>";
-	   if($zerotoforty!=null){
-       echo "****".'<br>';
-       echo "Your appropriate coats:".'<br>';
-       for($ab=0; $ab<count($zerotoforty); $ab++){
-         echo $zerotoforty[$ab];
-         if($ab!=count($zerotoforty)-1){
-           echo ", ";
-         }
-         else {
-           echo ""."<br>";
-         }
-       }
-	   }
-     }
-     elseif($currTemp>40&&$currTemp<=60){
-       echo "wear a light jacket"."<br>";
-	   if($fortytosixty!=null){
-       echo "****".'<br>';
-       echo "Your appropriate coats:".'<br>';
-       for($ac=0; $ac<count($fortytosixty); $ac++){
-         echo $fortytosixty[$ac];
-         if($ac!=count($fortytosixty)-1){
-           echo ", ";
-         }
-         else {
-           echo ""."<br>";
-         }
-       }
-	   }
-     }
-     elseif($currTemp>60&&$currTemp<=80){
-       echo "It's pretty warm"."<br>";
-	   if($sixtytoeighty!=null){
-       echo "****".'<br>';
-       echo "Your appropriate coats:".'<br>';
-       for($ad=0; $ad<count($sixtytoeighty); $ad++){
-         echo $sixtytoeighty[$ad];
-         if($ad!=count($sixtytoeighty)-1){
-           echo ", ";
-         }
-         else {
-           echo ""."<br>";
-         }
-       }
-	   }
-     }
-     else{
-       echo "It's hot out."."<br>";
-	   if($eightyplus!=null){
-       echo "****".'<br>';
-       echo "Your appropriate coats:".'<br>';
-       for($ae=0; $ae<count($eightyplus); $ae++){
-         echo $eightyplus[$ae];
-         if($ae!=count($eightyplus)-1){
-           echo ", ";
-         }
-         else {
-           echo ""."<br>";
-         }
-       }
-	   }
-     }
-   }
-   if($r == 2){
-     echo '<id = "weather">'.$endArray[$m][$r]."<br>";
-   }
-   if($r == 3){
-     echo '<img src="'.$endArray[$m][$r].'" alt="unable to load image" />';
-   }
-   if($r == 4){
-     echo '<id = "text">'.$endArray[$m][$r]."</div><br>";
-   }
-   //echo $endArray[$m][$r]."<br>";
-   echo " ";
- }
- echo "****"."<br>";
-}
-?>
-
-<form method="post" action="inputClothes.php">
+		$url="http://forecast.weather.gov/MapClick.php?lat=".$lat."&lon=".$lon."&FcstType=json";
+		//echo "URL: ".$url."<br>";
+		$curl_handle=curl_init();
+		curl_setopt($curl_handle, CURLOPT_URL,$url);
+		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+		$query = curl_exec($curl_handle);
+		curl_close($curl_handle);
+		//echo $query;
+		$Jweather = json_decode($query);
+		$startPeriodArray = $Jweather->time->startPeriodName;
+		$temp = $Jweather->data->temperature;
+		$weather = $Jweather->data->weather;
+		$icon = $Jweather->data->iconLink;
+		$size = sizeof($startPeriodArray);
+		$text = $Jweather->data->text;
+		$tempId = $Jweather->time->tempLabel;
+		$endArray = array(null, null, null, null, null, null, null);
+		$newArray;
+		$counter=0;
+		for($i = 0; $i < 12; $i++){
+			$newArray = array($startPeriodArray[$i], $temp[$i], $weather[$i], $icon[$i], $text[$i]);
+			$endArray[$i] = $newArray;
+		}
+		for( $m=0; $m<8; $m++){
+		?> 
+		<section>
+		<?php
+		
+			for($r=0; $r<10; $r++){
+				if($r == 0){
+				
+					echo '<div type="text" id= "day" ><h3>'.$endArray[$m][$r]."</div></h3><br>";
+			
+				}
+				if($r == 1){
+					echo '<div type="text" id="temp"><b><i>'.$tempId[$m], ": ".'</b></div></i>';
+					echo '<div type="text" id = "temperature">'.$endArray[$m][$r]."&deg;</div><br>";
+				if($endArray[$m][$r]<30){
+					echo "<b><center>Wear a heavy coat."."</center></b><br>";
+				}
+				elseif($endArray[$m][$r]<50){
+					echo "<b><center>Wear a medium jacket."."</center></b><br>";
+				}
+				elseif($endArray[$m][$r]<60){
+					echo "<b><center>Wear a light jacket."."</b></center><br>";
+				}
+				else{
+					echo "<b><center>You do not need to wear a jacket today."."</center></b><br>";
+				}
+			}
+			if($r == 2){
+				echo '<font size="2"><i><center><id = "weather">'.$endArray[$m][$r]."</i></font></center>";
+			}
+			if($r == 3){
+				echo '<p><img src="'.$endArray[$m][$r].'" alt="unable to load image" style="width:400px; height: auto; margin:0 auto; display:block";  /></p><br>';
+			}
+			if($r == 4){
+				echo '<br><b><i><div type="text" id = "text">'.$endArray[$m][$r]."</div></b></i><br>";
+			}
+		//echo $endArray[$m][$r]."<br>";
+		echo " ";
+		}
+		?> 
+		</section>
+		<?php
+		echo " "."<br>";
+	}
+	?>
+	<form method="post" action="inputClothes.php">
   Enter jackets:
    <input type = "submit" id="button" style= "color: #cc99ff; background-color: #0066ff; opacity: 0.5;" >
 
 </form>
-</body>
+	</body>
 </html>
